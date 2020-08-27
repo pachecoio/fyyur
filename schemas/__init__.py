0b057a648b1d808e22958d0c953ba24a1f780603
Thiago Pacheco <thiagodelimapacheco@gmail.com>
2020-08-26 20:15:17 -0400

    Integrated edit form

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -24,6 +24,18 @@ class BaseSchema(ma.Schema):
     genres = fields.List(fields.Nested(GenreSchema))
 
 
+class VenueShowSchema(ma.Schema):
+    id = fields.Integer()
+    artist_name = fields.Method('build_artist_name')
+    artist_image_link = fields.Method('build_artist_image_link')
+    start_time = fields.DateTime()
+
+    def build_artist_name(self, obj):
+        return obj.artist.name
+
+    def build_artist_image_link(self, obj):
+        return obj.artist.image_link
+
 class VenueSchema(BaseSchema):
     id = fields.Integer(required=True)
     name = fields.String(required=True)
@@ -31,7 +43,25 @@ class VenueSchema(BaseSchema):
     state = fields.String(required=True)
     address = fields.String(required=True)
     phone = fields.String(required=True)
-    genres = fields.Method('build_genres', deserialize='load_genres')
+    genres = fields.Method('build_genres')
+    website = fields.String()
+    facebook_link = fields.String(required=True)
+    seeking_talent = fields.Boolean()
+    seeking_description = fields.String()
+    image_link = fields.String()
+    past_shows = fields.List(
+        fields.Nested(VenueShowSchema),
+        attribute="shows"
+    )
+    # upcoming_shows = past_shows = fields.List(
+    #     fields.Nested(VenueShowSchema),
+    #     attribute="shows"
+    # )
+
+    def build_genres(self, obj):
+        return [
+            genre.name for genre in obj.genres
+        ]
 
 
 class VenueCreateSchema(BaseSchema):
@@ -42,6 +72,10 @@ class VenueCreateSchema(BaseSchema):
     phone = fields.String(required=True)
     genres = fields.Method('build_genres', deserialize='load_genres')
     city = fields.String(required=True)
+    facebook_link = fields.String()
+    website = fields.String()
+    seeking_description = fields.String()
+    seeking_talent = fields.Boolean()
 
     def build_genres(self, obj):
         return obj.genres
